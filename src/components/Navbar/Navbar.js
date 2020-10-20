34ee5c22fb368b6b2066de352b42e1725399b10f
jaaska-dev <jaaska.dev@gmail.com>
2020-10-20 23:43:02 +0300

    Styling changes to the Navigation bar and Button.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,10 @@
 import React, {useState, Fragment} from 'react';
 import {Link} from 'react-router-dom';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Button from '../Button/Button'
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const clickHandler = () => {
         setClick(!click);
@@ -12,6 +13,18 @@ const Navbar = () => {
     const closeMenuOnMobile = () => {
         setClick(false);
     }
+
+    //A function that removes the button and displays it depending on the screen size
+    const showButton = () => {
+        if (window.innerWidth <<= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    //Whenever the window resizes, showButton() is invoked
+    window.addEventListener('resize', showButton);
 
     return (
         <Fragment>
@@ -23,29 +36,32 @@ const Navbar = () => {
                     <div className="menu-icon" onClick={clickHandler}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
                     </div>
+
+                    <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to='/' className='nav-links' onClick={closeMenuOnMobile}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/services' className='nav-links' onClick={closeMenuOnMobile}>
+                                About Us
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/products' className='nav-links' onClick={closeMenuOnMobile}>
+                                Our Services
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMenuOnMobile}>
+                                Sign Up
+                            </Link>
+                        </li>
+                    </ul>
+                    {/*If button is true, returns the Button component*/}
+                    {button && <Button buttonStyle={'btn--outline'}>Sign Up</Button>}
                 </div>
-                <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
-                    <li className="nav-item">
-                        <Link to='/' className='nav-links' onClick={closeMenuOnMobile}>
-                            Home
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to='/services' className='nav-links' onClick={closeMenuOnMobile}>
-                            About Us
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to='/products' className='nav-links' onClick={closeMenuOnMobile}>
-                            Our Services
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMenuOnMobile}>
-                            Sign Up
-                        </Link>
-                    </li>
-                </ul>
             </nav>
         </Fragment>
     );
