a4088d1c4ea0b85ae686f8bee0ba391ff794dd05
jaaska-dev <jaaska.dev@gmail.com>
2020-10-21 00:46:24 +0300

    Navigation style bug fixed.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,37 +1,35 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import './Button.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-//Button styling array
-const STYLES = ['btn--primary', 'btn--outline'];
+const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
 
-//An array for button sizes
 const SIZES = ['btn--medium', 'btn--large'];
 
-const Button = (
-    {
-        children,
-        type,
-        onClick,
-        buttonStyle,
-        buttonSize
-    }) => {
-    //If the button has a style, it will leave it, otherwise first style in the STYLES array
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
-    //The same with size as with styles.
+export const Button = ({
+                           children,
+                           type,
+                           onClick,
+                           buttonStyle,
+                           buttonSize
+                       }) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle)
+        ? buttonStyle
+        : STYLES[0];
+
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
     return (
-        <Link to='/' className='btn-mobile'>
+        <Link to='/sign-up' className='btn-mobile'>
             <button
                 className={`btn ${checkButtonStyle} ${checkButtonSize}`}
                 onClick={onClick}
                 type={type}
-                >
+            >
                 {children}
             </button>
         </Link>
-    )
-}
+    );
+};
 
-export default Button;
+
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, {useState, Fragment} from 'react';
 import {Link} from 'react-router-dom';
-import Button from '../Button/Button'
+import { Button } from '../Button/Button'
 import './Navbar.css';
 
 const Navbar = () => {
@@ -17,7 +17,7 @@ const Navbar = () => {
 
     //A function that removes the button and displays it depending on the screen size
     const showButton = () => {
-        if (window.innerWidth <<= 960) {
+        if (window.innerWidth <= 960) {
             setButton(false);
         } else {
             setButton(true);
@@ -33,12 +33,13 @@ const Navbar = () => {
                 <div className="navbar-container">
                     <Link to='/' className='navbar-logo'>
                         by karl
+                        <i className='fab fa-typo3' />
                     </Link>
                     <div className="menu-icon" onClick={clickHandler}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
                     </div>
 
-                    <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className="nav-item">
                             <Link to='/' className='nav-links' onClick={closeMenuOnMobile}>
                                 Home
