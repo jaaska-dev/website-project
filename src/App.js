2d23cb44d35c3295c3266fd50c60aed25ca5b57e
jaaska-dev <jaaska.dev@gmail.com>
2020-10-20 22:54:37 +0300

    Button component added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            <h1>Website Project!</h1>
-        </div>
+       <Fragment>
+           <Router>
+               <Navbar />
+               <Switch>
+                   <Route path='/' exact />
+               </Switch>
+           </Router>
+       </Fragment>
+
     );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,54 @@
 import React, {useState, Fragment} from 'react';
+import {Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-const navbar = () => {
+const Navbar = () => {
+    const [click, setClick] = useState(false);
+
+    const clickHandler = () => {
+        setClick(!click);
+    }
+
+    const closeMenuOnMobile = () => {
+        setClick(false);
+    }
+
     return (
-       <Fragment>
-           <nav className="navbar">
-               <div className="navbar-container">
-                <Link to='/' className='navbar-logo'>
-                    Karl Marten J.
-                </Link>
-               </div>
-           </nav>
-       </Fragment>
+        <Fragment>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to='/' className='navbar-logo'>
+                        Karl Marten J.
+                    </Link>
+                    <div className="menu-icon" onClick={clickHandler}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                    </div>
+                </div>
+                <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
+                    <li className="nav-item">
+                        <Link to='/' className='nav-links' onClick={closeMenuOnMobile}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/services' className='nav-links' onClick={closeMenuOnMobile}>
+                            About Us
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/products' className='nav-links' onClick={closeMenuOnMobile}>
+                            Our Services
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMenuOnMobile}>
+                            Sign Up
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </Fragment>
     );
 };
 
-export default navbar;
+export default Navbar;
